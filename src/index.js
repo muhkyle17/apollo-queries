92f39471d83ecd813f1199b9ff870c1e8b884ae0
Mikyle Reyes <mikylereyes@Mikyles-MacBook-Air-2.local>
2022-02-07 18:47:47 +0800

    Execute a simple query

--- src/index.js
+++ src/index.js
@@ -1,17 +1,79 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'https://71z1g.sse.codesandbox.io/',
+  cache: new InMemoryCache(),
+})
+
+const GET_DOGS = gql`
+  query GetDogs {
+    dogs {
+      id
+      breed
+    }
+  }
+`
+
+client
+  .query({
+    query: gql`
+      query GetDogs {
+        dogs {
+          id
+          breed
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result))
+
+function Dogs({ onDogSelected }) {
+  const { loading, error, data } = useQuery(GET_DOGS)
+
+  if (loading) return 'Loading...'
+  if (error) return `Error! ${error.message}`
+
+  return (
+    <select name='dog' onChange={onDogSelected}>
+      {data.dogs.map((dog) => (
+        <option key={dog.id} value={dog.breed}>
+          {dog.breed}
+        </option>
+      ))}
+    </select>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <Dogs />
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+    ,
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
